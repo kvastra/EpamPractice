18f153b0e6d593d0df2f186868b7c6949a65dbd4
kvastra <kon.valeria.s@gmail.com>
2026-04-23 13:49:15 +0400

    Add some code for Task2

---
+++ Task2/Classes/Round.cs
@@ -0,0 +1,37 @@
+﻿using System.Drawing;
+
+namespace Task2.Classes;
+
+public class Round
+{
+    /// <summary>
+    /// Координаты центра окружности
+    /// </summary>
+    private Point Center { get; set; }
+
+    /// <summary>
+    /// Радиус окружности
+    /// </summary>
+    private int Radius { get; set; }
+
+    public Round(int x, int y, int radius)
+    {
+        if (radius <= 0)
+            throw new Exception("Radius must have a positive value.");
+        
+        Center = new Point(x, y);
+        Radius = radius;
+    }
+    
+    /// <summary>
+    /// Длина окружности
+    /// </summary>
+    /// <returns></returns>
+    public double Length => 2 * Math.PI * Radius;
+
+    /// <summary>
+    /// Площадь окружности
+    /// </summary>
+    /// <returns></returns>
+    public double Square => Math.PI * Radius * Radius;
+}
---
+++ Task2/Classes/Triangle.cs
@@ -0,0 +1,30 @@
+﻿namespace Task2.Classes;
+
+public class Triangle
+{
+    private double SideA { get; set; }
+    
+    private double SideB { get; set; }
+    
+    private double SideC { get; set; }
+
+    public Triangle(double a, double b, double c)
+    {
+        if (a <= 0 || b <= 0 || c <= 0)
+            throw new Exception("All sides must have positive values.");
+
+        if (a + b > c || a + c > b || b + c > a)
+            throw new Exception("Incorrect value of sides: the sum of two sides must not be greater than the third.");
+
+        (SideA, SideB, SideC) = (a, b, c);
+    }
+
+    public double Perimeter => SideA + SideB + SideC;
+
+    public double Square()
+    {
+        var p = Perimeter / 2;
+
+        return p * (p - SideA) * (p - SideB) * (p - SideC);
+    }
+}

39f289a19b7ffbd7aa4e982c30177acd634e6c10
kvastra <kon.valeria.s@gmail.com>
2026-04-23 12:54:46 +0400

    Task 1.05

--- Task1.5/Program.cs
+++ Task1.5/Program.cs
@@ -1,29 +1,76 @@
-﻿using Tools;
+﻿using System.Text;
+using Tools;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.Write("Input array dimension: ");
-        if (!int.TryParse(Console.ReadLine().Trim(), out var arrayDimension))
-            return;
+        var arrayDimension = DigitsTools.ReadLineAndParseToInt();
 
-        var subArraysDimensions = new int[arrayDimension];
+        if (arrayDimension == null || !DigitsTools.ValidateNumber(arrayDimension.Value)) return;
+
+        var subArraysDimensions = new int[arrayDimension.Value];
         var random = StaticRandom.Random;
         var count = 1;
 
-        for (var i = 0; i < arrayDimension; i++)
+        for (var i = 0; i < arrayDimension.Value; i++)
         {
             Console.Write($"Input sub array [{i}] dimension: ");
-            if (!int.TryParse(Console.ReadLine().Trim(), out var subArrayDim))
-                return;
+            var subArrayDim = DigitsTools.ReadLineAndParseToInt();
+            
+            if (subArrayDim == null || !DigitsTools.ValidateNumber(subArrayDim.Value)) return;
 
-            subArraysDimensions[i] = subArrayDim;
-            count *= subArrayDim;
+            subArraysDimensions[i] = subArrayDim.Value;
+            count *= subArrayDim.Value;
         }
 
-        var array = ArrayTools.FillArrayRandomValues(new int[count], 0, 100);
+        var array = (int[])ArrayTools.FillArrayRandomValues(new int[count], 0, 100);
         
+        Console.WriteLine(MakeStringFromArray(array, subArraysDimensions));
+
+        array = SortingTools.TreeSort(array);
         
+        Console.WriteLine(MakeStringFromArray(array, subArraysDimensions));
+    }
+
+    static string MakeStringFromArray(Array array, int[] dimensions)
+    {
+        var sb = new StringBuilder();
+        var start = 0;
+        MakeStringFromArrayRec(array, dimensions.Length, dimensions, sb, ref start);
+        return sb.ToString();
+    }
+
+    static void MakeStringFromArrayRec(Array array, int currentRank, int[] dimensions, StringBuilder output, ref int start)
+    {
+        output.Append("[");
+        if (currentRank == 1)
+        {
+            for (var i = start; i < start + dimensions[^1]; i++)
+            {
+                output.Append(array.GetValue(i));
+
+                if (i == start + dimensions[^1] - 1)
+                    break;
+
+                output.Append(", ");
+            }
+
+            start += dimensions[^1];
+        }
+        else
+        {
+            for (var i = 0; i < dimensions[^currentRank]; i++) {
+                MakeStringFromArrayRec(array, currentRank - 1, dimensions, output, ref start);
+                
+                if (i == dimensions[^currentRank] - 1)
+                    break;
+
+                output.Append(", ");
+            }
+        }
+        
+        output.Append("]");
     }
 }
